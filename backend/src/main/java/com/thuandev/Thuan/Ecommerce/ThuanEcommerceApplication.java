7ed54c9458b611ffb4553ae49d33eb9438a0b8b8
THUAN\tranm <thuant1002@gmail.com>
2025-05-17 19:24:14 +0700

    Setting backend project

---
+++ backend/src/main/java/com/thuandev/Thuan/Ecommerce/ThuanEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.thuandev.Thuan.Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThuanEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThuanEcommerceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thuandev/Thuan/Ecommerce/ThuanEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thuandev.Thuan.Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThuanEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
